cdc41de9af04047d6c5a8f0b8bfb15c46ec09867
amirhosein <ah.zamani00@gmail.com>
2023-02-25 19:58:02 +0330

    Even numbers between two numbers finished

---
+++ script.js
@@ -0,0 +1,23 @@
+let $ = document;
+let FirstNumber = $.querySelector(".FirstNumber");
+let SecondNumber = $.querySelector(".SecondNumber");
+let btn = $.querySelector(".btn");
+let showResult = $.querySelector(".showResult");
+
+btn.addEventListener("click", () => {
+  let receiveNum1 = Number(FirstNumber.value)
+  let receiveNum2 = Number(SecondNumber.value)
+
+  if (receiveNum1 % 2 === 0) {
+    while (receiveNum1 < receiveNum2) {
+      console.log(receiveNum1);
+      receiveNum1 += 2;
+    }
+  } else {
+    receiveNum1++;
+    while (receiveNum1 < receiveNum2) {
+      console.log(receiveNum1);
+      receiveNum1 += 2;
+    }
+  }
+});
